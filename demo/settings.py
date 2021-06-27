3de3219d2b1428abbf54df9ca68e12affce1cc20
mohamed <mohammed.elasar@gmail.com>
2021-06-27 13:18:33 +0300

    login successfully and adding crispy forms

--- demo/settings.py
+++ demo/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -88,3 +90,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+
+# for crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
